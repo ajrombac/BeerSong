188af4054d71e3de7386c6600849343686fb20dc
Alex Rombach <ajrombac@oakland.edu>
2016-02-03 18:32:01 -0500

    Added logic for funtion Ninety_Nine_Bottles_of_Beer(); in BeerSong.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,21 @@ public class Main {
         // Calls Ninety_Nine_Bottles_of_Beer();
     }
     public static void Ninety_Nine_Bottles_of_Beer(){
-        // Prints song
+        for(int i=99; i>0; i--){
+
+            if (i > 2){
+                System.out.println((i) + " bottles of beer on the wall, " + (i) + " bottles of beer");
+                System.out.println("take one down, pass it around, " + (i - 1) + " bottles of beer on the wall.");
+            }
+            else if (i == 2){
+                System.out.println((i) + " bottles of beer on the wall, " + (i) + " bottles of beer");
+                System.out.println("take one down, pass it around, " + (i - 1) + " bottle of beer on the wall.");
+            }
+            else if (i == 1){
+                System.out.println((i) + " bottle of beer on the wall, " + (i) + " bottle of beer");
+                System.out.println("take one down, pass it around, no more bottles of beer on the wall." );
+            }
+        }
     }
 
 }
