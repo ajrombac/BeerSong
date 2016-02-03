1cacbf23b0c5b994c0b4a927258a6f0cf0e50160
Alex Rombach <ajrombac@oakland.edu>
2016-02-03 18:33:20 -0500

    Added way to call function Ninety_Nine_Bottles_of_Beer(); from the main function in BeerSong.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        // Calls Ninety_Nine_Bottles_of_Beer();
+        Ninety_Nine_Bottles_of_Beer();
     }
     public static void Ninety_Nine_Bottles_of_Beer(){
         for(int i=99; i>0; i--){
