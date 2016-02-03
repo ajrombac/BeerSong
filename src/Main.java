b9513d33f3fa4a214818a7f03b247897347c0c6c
Alex Rombach <ajrombac@oakland.edu>
2016-02-03 18:26:47 -0500

    Added initial skeliton project files including BeerSong.Java and other dependencies

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Calls Ninety_Nine_Bottles_of_Beer();
+    }
+    public static void Ninety_Nine_Bottles_of_Beer(){
+        // Prints song
+    }
+
+}
+
+
